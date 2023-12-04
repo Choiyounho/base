5e3cb2e0c1f7d8ed1046652473d23701d6cf1f85
Choiyounho <maxosa78@gmail.com>
2023-12-04 15:54:06 +0900

    refator: 파라미터 명 변경 적용

--- app/src/main/java/com/soten/base/BaseItemDiffUtil.kt
+++ app/src/main/java/com/soten/base/BaseItemDiffUtil.kt
@@ -5,7 +5,7 @@ import androidx.recyclerview.widget.DiffUtil
 class BaseItemDiffUtil : DiffUtil.ItemCallback<BaseItem>() {
 
     override fun areItemsTheSame(oldItem: BaseItem, newItem: BaseItem): Boolean {
-        return oldItem.name == newItem.name
+        return oldItem.content == newItem.content
     }
 
     override fun areContentsTheSame(oldItem: BaseItem, newItem: BaseItem): Boolean {
