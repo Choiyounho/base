2a6f8b6cbdc6a0c643b6518d819b02315f5dc4e4
Choiyounho <maxosa78@gmail.com>
2023-12-04 15:55:01 +0900

    feat: BaseFragment 구현

---
+++ app/src/main/java/com/soten/base/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.soten.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+open class BaseFragment<DB : ViewDataBinding>(
+    private val layoutRes: Int
+) : Fragment() {
+
+    private var _binding: DB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
